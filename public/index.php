9a47eef600a9dddd67a50c0de1838c552b06d878
Yosko <yosko@yosko.net>
2026-04-29 11:11:31 +0200

    no need for explicit root path anymore

--- public/index.php
+++ public/index.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \App\Application(devEnv: true, root: '../');
+$app = new \App\Application(devEnv: true);
 $app->run();
